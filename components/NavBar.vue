aac130c44a01f1290a55fcf8aebd1ab440b3ec81
den <katatsu12@gmail.com>
2019-10-30 13:51:46 +0200

    Add About page, update btn in tool bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,14 +4,6 @@
       v-toolbar-title Amazing Trip
       v-spacer
       v-toolbar-items
-        v-btn(text)
-          nuxt-link(to='/') Home
-        v-btn(text)
-          nuxt-link(to='/about') About
+        v-btn(text to="/" nuxt) Home
+        v-btn(text to="/about" nuxt) About
 </template>
-
-<style scoped lang="sass">
-a
-  text-decoration: none
-  color: black
-</style>
